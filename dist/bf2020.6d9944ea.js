101c0458913f5509a0345d61a45d3f38cb763c26
Craig Francies <craig@crazydesigns.co.uk>
2020-11-04 13:29:43 +0000

    added ie11 fix

--- dist/bf2020.6d9944ea.js
+++ dist/bf2020.6d9944ea.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./styles.scss");
 },{"./styles.scss":"I4sy"}]},{},["Focm"], null)
-//# sourceMappingURL=/bf2020.c490a224.js.map
+//# sourceMappingURL=/bf2020.6d9944ea.js.map
